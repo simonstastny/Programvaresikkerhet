7380e2e7820b0a90a9c8f5c1c5e85815c1371890
Antoine AUGER <antoine.auger1@gmail.com>
2012-10-23 16:10:34 +0200

    problem fixed on a servlet

---
+++ assignment_2/LUT-Improved/src/servlets/ServletFormAddCountrySchool.java
@@ -0,0 +1,19 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.Country;
+import forms.FormCountry;
+
+public class ServletFormAddCountrySchool extends HttpServlet {
+    public static final String VIEW = "/WEB-INF/formAddCountrySchool.jsp";
+		
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+        this.getServletContext().getRequestDispatcher( VIEW ).forward( request, response );
+    }
+}
