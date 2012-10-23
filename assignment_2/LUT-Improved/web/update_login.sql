2678137468674c2509463fb28d41d9db3988214a
Simon Stastny <stastny.simon@gmail.com>
2012-10-23 16:15:57 +0200

    passwords are stored as bcrypt hash

--- assignment_2/LUT-Improved/web/update_login.sql
+++ assignment_2/LUT-Improved/web/update_login.sql
@@ -2,12 +2,12 @@ drop table users;
 
 create table users(
 login varchar(26) NOT NULL,
-password varchar(26) NOT NULL,
+password varchar(128) NOT NULL,
 PRIMARY KEY (login)
 );
 
-insert into users values ('adrienoa', 'azerty');
-insert into users values ('aubertad','toto');
+insert into users values ('adrienoa', '$2a$13$xkJmx2SdSUAAcdm2Thj1dulqsHIgtw.HZEDagr/4AkhyZApeCYxNi');
+insert into users values ('aubertad','$2a$13$JV7Q6U2w5rditz6n6Lev7OvIWoUmIrarPk9I1TiMgYCqVOY..NWLW');
 
 drop table admin_users;
 
